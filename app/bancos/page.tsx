2ac8f0c4ba5a15eaebb505b17f33f5a2c5ee21bf
Nelly Valde <valderramanelly722@gmail.com>
2026-06-23 15:42:18 -0500

    Fix: actualizar version worker pdfjs a 4.4.168

--- app/bancos/page.tsx
+++ app/bancos/page.tsx
@@ -109,7 +109,7 @@ export default function BancosPage() {
 
   const leerPDF = async (file: File, config: BancoConfig): Promise<MovimientoBanco[]> => {
     const pdfjsLib = await import('pdfjs-dist')
-    pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js`
+    pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.worker.min.mjs`
     const arrayBuffer = await file.arrayBuffer()
     const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise
     const movs: MovimientoBanco[] = []
